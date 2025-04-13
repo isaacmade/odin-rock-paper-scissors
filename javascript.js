63aff997cc94c5a0cbabf4d130b16c953404b4a5
Isaac Key <175264936+isaacmade@users.noreply.github.com>
2025-04-14 08:03:20 +1000

    written function for getComputerChoice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
-console.log("name jef")
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3 + 1);
+    
+    if (randomNumber === 1) {
+        return "rock";
+    } else if (randomNumber === 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
---
+++ psuedo.md
@@ -0,0 +1,8 @@
+function for getComputerChoice
+
+randomly roll a number between 1 and 3
+if number lands on 1, assign to rock
+lands on 2, assign to paper
+lands on 3, assign to scissors
+
+return the string value 
