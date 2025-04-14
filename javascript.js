225c1d0961f7e716194c62b99358e7622af3f0ac
Isaac Key <175264936+isaacmade@users.noreply.github.com>
2025-04-15 08:02:19 +1000

    playRound written, basic game round

--- javascript.js
+++ javascript.js
@@ -1,5 +1,8 @@
+let computerScore = 0;
+let humanScore = 0;
+
 function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random() * 3 + 1);
+    const randomNumber = Math.floor(Math.random() * 3 + 1);
     
     if (randomNumber === 1) {
         return "rock";
@@ -10,3 +13,39 @@ function getComputerChoice() {
     }
 }
 
+function getHumanChoice() {
+    const playerChoice = prompt("scissors, paper, rock!!");
+    return playerChoice;
+}
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!");
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+        console.log("You lose! Paper beats rock!");
+        ++computerScore;
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log("You win! Rock beats scissors!");
+        ++humanScore;
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        console.log("You lose! Scissors beats paper!");
+        ++computerScore;
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log("You win! Paper beats rock!");
+        ++humanScore;
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        console.log("You lose! Rock beats scissors!");
+        ++computerScore;
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log("You win! Scissors beats paper!");
+        ++humanScore;
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
--- psuedo.md
+++ psuedo.md
@@ -1,8 +1,21 @@
-function for getComputerChoice
+#### function for getComputerChoice()
 
-randomly roll a number between 1 and 3
-if number lands on 1, assign to rock
-lands on 2, assign to paper
-lands on 3, assign to scissors
+- randomly roll a number between 1 and 3
+- if number lands on 1, assign to rock
+- lands on 2, assign to paper
+- lands on 3, assign to scissors
 
-return the string value 
+return the string value
+
+
+#### function for getHumanChoice()
+
+- prompt for a user input
+- user enters either rock paper or scissors
+
+
+#### function for playRound()
+
+- convert both arguments to lowercase
+
+- write if/and conditionals?
